9c5345d6d7def9db26792e7d34a230c9b0afd1b7
lzq <2634512249@qq.com>
2024-02-27 12:15:46 +0800

    刘泽琦第一次提交

---
+++ JS/1.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+    var cube = document.querySelectorAll('.cube');
+
+    for (var i = 0; i < cube.length; i++) {
+        cube[i].onclick = function () {
+            // alert(111);
+            for (var i = 0; i < cube.length; i++) {
+            cube[i].style.border = '';
+        }
+            this.style.border = "3px solid #5768C3";
+        }
+    }
+})
