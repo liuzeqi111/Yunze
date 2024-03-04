dff9a1d0ac9e1b8dea3a22d0ae794e9c6056ce58
lzq <2634512249@qq.com>
2024-03-04 23:02:21 +0800

    刘泽琦第五次提交

--- 刘泽琦/JS/1.js
+++ 刘泽琦/JS/1.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', function () {
     var cube = document.querySelectorAll('.cube');
+    var manager = document.querySelector('.manager');
+    var student = document.querySelector('.student');
+    var btn = document.querySelector('.btn');
 
     for (var i = 0; i < cube.length; i++) {
         cube[i].onclick = function () {
@@ -9,4 +12,15 @@ window.addEventListener('load', function () {
             this.style.border = "3px solid #5768C3";
         }
     }
+    
+    if (manager.style.border = "3px solid #5768C3") {
+        btn.onclick = function () {
+            location.href = "../../裴润涵/HTML/登录(管理).html"
+        }
+    }else if (student.style.border = "3px solid #5768C3") {
+        btn.onclick = function () {
+            location.href = "../../裴润涵/HTML/登录(学生).html"
+        }
+    }
+    
 })
