acb66906da02588744a21bf2c918e01a16927511
反求诸己 <13765722+PEIRUNHAN@user.noreply.gitee.com>
2024-03-12 18:30:00 +0800

    全栈-裴润涵-第n次提交

--- 刘泽琦/JS/1.js
+++ 刘泽琦/JS/1.js
@@ -1,26 +1,27 @@
-window.addEventListener('load', function () {
-    var cube = document.querySelectorAll('.cube');
-    var manager = document.querySelector('.manager');
-    var student = document.querySelector('.student');
-    var btn = document.querySelector('.btn');
-
-    for (var i = 0; i < cube.length; i++) {
-        cube[i].onclick = function () {
-            for (var i = 0; i < cube.length; i++) {
-            cube[i].style.border = '';
-        }
-            this.style.border = "3px solid #5768C3";
-        }
-    }
-    
-    if (manager.style.border = "3px solid #5768C3") {
-        btn.onclick = function () {
-            location.href = "../../裴润涵/HTML/登录(管理).html"
-        }
-    }else if (student.style.border = "3px solid #5768C3") {
-        btn.onclick = function () {
-            location.href = "../../裴润涵/HTML/登录(学生).html"
-        }
-    }
-    
-})
+window.addEventListener('load', function () {  
+    var cubes = document.querySelectorAll('.cube');  
+    var btn = document.querySelector('.btn');  
+  
+    cubes.forEach(function(cube) {  
+        cube.onclick = function () {  
+            // 移除所有 cube 的边框  
+            cubes.forEach(function(otherCube) {  
+                otherCube.style.border = '';  
+            });  
+  
+            // 为被点击的 cube 添加边框  
+            this.style.border = "3px solid #5768C3";  
+  
+            // 根据被点击的 cube 的类名设置 btn 的点击行为  
+            if (this.classList.contains('manager')) {  
+                btn.onclick = function () {  
+                    location.href = "../../裴润涵/HTML/登录(管理).html";  
+                };  
+            } else if (this.classList.contains('student')) {  
+                btn.onclick = function () {  
+                    location.href = "../../裴润涵/HTML/登录(学生).html";  
+                };  
+            }  
+        };  
+    });  
+});
--- 鲍若康/学生信息-学生端.js
+++ 鲍若康/学生信息-学生端.js
@@ -1,36 +1,95 @@
+// var pare = window.opener
+// var six = pare.document.getElementById("search")
+var names = document.getElementById('name')
+var age = document.getElementById('age')
+var major = document.getElementById('major')
+var position = document.getElementById('position')
+var id=document.getElementById('id')
+var direction = document.getElementById('direction')
+var award = document.getElementById('award')
 
-var input = document.querySelectorAll('input');
-var award_hint= document.querySelector('textarea');
+var question = document.querySelector('.question')
+question.onclick = () => {
+    alert('功能还在开发中......')
+}
+// function fun() {
+//     axios.post('http://127.0.0.1:9000/search', Qs.stringify(data))
+//         .then((res) => {
+//             console.log(res.data)
+//             names.value = res.data[0]
+//             age.value = res.data[1]
+//             major.value = res.data[2]
+//             id.value = res.data[3]
+//             position.value = res.data[4]
+//             direction.value=res.data[5]
+//     })
+// }
+// fun()
+
+// var input = document.querySelectorAll('input');
+// var award_hint= document.querySelector('textarea');
 var change = document.querySelector('.change');
 var admit = document.querySelector('.admit');
-var myselect = document.querySelector('select');
-var index = myselect.selectedIndex;
+// var myselect = document.querySelector('select');
+// var index = myselect.selectedIndex;
 //跳转页面
-var page_jump = document.querySelectorAll('li');
-page_jump[0].onclick = function () {
-    alert('0')
-}
-page_jump[1].onclick = function () {
-    alert('1')
-}
-page_jump[2].onclick = function () {
-    alert('2')
-}
-page_jump[3].onclick = function () {
-    alert('3')
-}
+// var page_jump = document.querySelectorAll('li');
+// page_jump[0].onclick = function () {
+//     alert('0')
+// }
+// page_jump[1].onclick = function () {
+//     alert('1')
+// }
+// page_jump[2].onclick = function () {
+//     alert('2')
+// }
+// page_jump[3].onclick = function () {
+//     alert('3')
+// }
 //信息更改
-   change.onclick=function(){
-   for(let i=0;i<6;i++){
-       input[i].value = '';
-    }
+    change.onclick=function(){
+        alert('功能暂时无法使用')
 }
    //提交信息
 admit.onclick = function () {
-         for(let i=0;i<5;i++){
-             console.log(input[i].value)
-   }
-    console.log(myselect.options[index].text)
-    console.log(award_hint.value)
-        }
-
+let data = {
+    "name": names.value,
+    "age": age.value,
+    "major": major.value,
+    "position": position.value,
+    "id": id.value,
+    "direction": direction.value,
+    "award":award.value
+}
+    axios.post('http://127.0.0.1:9000/add', Qs.stringify(data))
+        .then((res) => {
+            if (res.data == 'yes') {
+                console.log(res.data)
+                alert("注册成功")
+                setTimeout(location.assign('../裴润涵/HTML/登录(学生).html'),300)
+            }
+            
+    })
+    }
+    // console.log(myselect.options[index].text)
+    // console.log(award_hint.value)
+        
+var pare = window.opener;
+var idcard = pare.document.querySelector('.id')
+function news() {
+    let data = {
+        'id':idcard.value
+    }
+    axios.post('http://127.0.0.1:9000/search1', Qs.stringify(data))
+        .then((res) => {
+        console.log(res.data)
+            names.value = res.data[0]
+            age.value = res.data[1]
+            major.value = res.data[2]
+            id.value = res.data[4]
+            position.value = res.data[3]
+            direction.value = res.data[5]
+            award.value=res.data[6]
+    })
+}
+news()
